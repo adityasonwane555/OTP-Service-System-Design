0bbdf9bdadec95ed9aabfd49dab0e319d2053416
adityasonwane555 <adityasonwane555@gmail.com>
2026-06-06 22:41:57 +0530

    feat: add OTP send controller

---
+++ src/controllers/otp.controller.js
@@ -0,0 +1,29 @@
+import { createOTP } from "../services/otp.service.js";
+
+export async function sendOTP(req, res) {
+  try {
+    const { target } = req.body;
+
+    if (!target) {
+      return res.status(400).json({
+        success: false,
+        message: "Target is required",
+      });
+    }
+
+    const result = await createOTP(target);
+
+    return res.status(201).json({
+      success: true,
+      ...result,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
