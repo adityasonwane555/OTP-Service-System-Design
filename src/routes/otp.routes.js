96ce5933734ca28948230dd51e9f6d87c318e05f
adityasonwane555 <adityasonwane555@gmail.com>
2026-06-06 22:42:10 +0530

    feat: create OTP API routes

--- src/routes/otp.routes.js
+++ src/routes/otp.routes.js
@@ -1,1 +1,9 @@
+import express from "express";
 
+import { sendOTP } from "../controllers/otp.controller.js";
+
+const router = express.Router();
+
+router.post("/send", sendOTP);
+
+export default router;
