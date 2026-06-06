b762c2bdfa59a83ee51124efd1ce636579cb58e0
adityasonwane555 <adityasonwane555@gmail.com>
2026-06-06 22:41:36 +0530

    feat: implement OTP generation utility

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,1 +1,8 @@
+export function generateOTP(length = 6) {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
 
+  return Math.floor(
+    min + Math.random() * (max - min + 1)
+  ).toString();
+}
