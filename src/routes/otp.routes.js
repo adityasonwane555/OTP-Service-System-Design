4b3469439651e063fe1918a8fa35d7bb1eadc4ca
adityasonwane555 <adityasonwane555@gmail.com>
2026-06-07 23:00:37 +0530

    feat: add OTP verification API route

--- src/routes/otp.routes.js
+++ src/routes/otp.routes.js
@@ -1,9 +1,17 @@
 import express from "express";
 
-import { sendOTP } from "../controllers/otp.controller.js";
+import {
+  sendOTP,
+  verifyOTPController
+} from "../controllers/otp.controller.js";
 
 const router = express.Router();
 
 router.post("/send", sendOTP);
+
+router.post(
+  "/verify",
+  verifyOTPController
+);
 
 export default router;
